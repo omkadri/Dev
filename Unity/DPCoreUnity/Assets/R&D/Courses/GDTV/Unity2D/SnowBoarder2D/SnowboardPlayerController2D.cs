2009e45a9d173d93232ff265c92e39cb089b8e24
Omar <omkadri@hotmail.com>
2024-10-05 13:43:20 -0400

    Added rotation to Snowboarder
    - Left and Right arrows now apply torque to the player rigidbody, rotating them left and right
    - also adjusted angular drag of player to rotate less frantically

--- Unity/DPCoreUnity/Assets/R&D/Courses/GDTV/Unity2D/SnowBoarder2D/SnowboardPlayerController2D.cs
+++ Unity/DPCoreUnity/Assets/R&D/Courses/GDTV/Unity2D/SnowBoarder2D/SnowboardPlayerController2D.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class SnowboardPlayerController2D : MonoBehaviour
 {
+    [SerializeField] float torqueAmount = 0.5f;
+    Rigidbody2D rb2d;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb2d.AddTorque(torqueAmount);
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            rb2d.AddTorque(-torqueAmount);
+        }
     }
 }
