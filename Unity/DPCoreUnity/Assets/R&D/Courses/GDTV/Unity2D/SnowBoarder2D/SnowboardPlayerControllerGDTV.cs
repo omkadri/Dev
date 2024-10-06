9b51e0411ec000083e9b7a3c8a16faddbf001e6c
Omar <omkadri@hotmail.com>
2024-10-05 22:20:33 -0400

    Created Win/Lose conditions for snowboarder game
    - Created Finish Line object with FinishLine.cs attached. It triggers when the player passes through it.
    - Added CrashDetector to Player object that is triggered by the head collider touching the floor.
    - Added folder for MarbleMaze prototype

--- Unity/DPCoreUnity/Assets/R&D/Courses/GDTV/Unity2D/SnowBoarder2D/SnowboardPlayerControllerGDTV.cs
+++ Unity/DPCoreUnity/Assets/R&D/Courses/GDTV/Unity2D/SnowBoarder2D/SnowboardPlayerControllerGDTV.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SnowboardPlayerController2D : MonoBehaviour
+public class SnowboardPlayerControllerGDTV : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 0.5f;
     Rigidbody2D rb2d;
