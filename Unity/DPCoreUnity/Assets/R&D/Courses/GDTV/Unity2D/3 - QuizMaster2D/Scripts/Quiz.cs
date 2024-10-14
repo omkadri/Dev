928939bf6b4ac9acc0d15f0cbf7302f9f85e8e45
Omar <omkadri@hotmail.com>
2024-10-14 04:49:23 -0400

    Add OnAnswerSelected() function to trigger result for selecting an answer
    - Created 2 Sprite vars: one for default ui sprite, and one for correct answer
    - Created OnAnswerSelected() function, which is triggered by the OnClick() callback in each of the 4 buttons.
    - Selecting the wrong answer will result in text changing to "Wrong + Correct Answer"
    - Selecting the correct anser will set the text to "Correct!!"

--- Unity/DPCoreUnity/Assets/R&D/Courses/GDTV/Unity2D/3 - QuizMaster2D/Scripts/Quiz.cs
+++ Unity/DPCoreUnity/Assets/R&D/Courses/GDTV/Unity2D/3 - QuizMaster2D/Scripts/Quiz.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
     [SerializeField] GameObject[] answerButtons;
+    int correctAnswerIndex;
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
 
     void Start()
     {
@@ -17,6 +21,26 @@ public class Quiz : MonoBehaviour
         {
             TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
             buttonText.text = question.GetAnswer(i);
+        }
+    }
+
+    public void OnAnswerSelected(int index)
+    {
+        Image buttonImage;
+        if(index == question.GetCorrectAnswerIndex())
+        {
+            questionText.text = "Correct!!!";
+            buttonImage = answerButtons[index].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+        else
+        {
+            correctAnswerIndex = question.GetCorrectAnswerIndex();
+            string correctAnswer = question.GetAnswer(correctAnswerIndex);
+            questionText.text = "Wrong!!!\n The Correct Answer is:\n" + correctAnswer;
+
+            buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
         }
     }
 }
