89f594f8a861252c7a66447dd9550d1785b4fbdb
Omar <omkadri@hotmail.com>
2024-10-15 02:55:04 -0400

    Added logic for Setting End Screen Text

--- Unity/DPCoreUnity/Assets/R&D/Courses/GDTV/Unity2D/3 - QuizMaster2D/Scripts/EndScreen.cs
+++ Unity/DPCoreUnity/Assets/R&D/Courses/GDTV/Unity2D/3 - QuizMaster2D/Scripts/EndScreen.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class EndScreen : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI finalScoreText;
+    ScoreKeeper scoreKeeper;
     void Start()
     {
-        
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
+    }
+
+    public void ShowFinalScore()
+    {
+        finalScoreText.text = "Quiz Completed!\nYou got a score of " + scoreKeeper.CalculateScore() + "%";
     }
 }
