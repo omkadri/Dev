08eab1e9f7bf84d9c2e397570b932dbb018ee9e0
Omar <omkadri@hotmail.com>
2024-10-15 03:55:35 -0400

    More Housekeeping

--- Unity/DPCoreUnity/Assets/R&D/Courses/GDTV/Unity2D/3 - QuizMaster2D/Scripts/QuizGameManagerGGTV.cs
+++ Unity/DPCoreUnity/Assets/R&D/Courses/GDTV/Unity2D/3 - QuizMaster2D/Scripts/QuizGameManagerGGTV.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
-public class QuizGameManager : MonoBehaviour
+public class QuizGameManagerGDTV : MonoBehaviour
 {
     QuizGDTV quiz;
     EndScreenQuizGGTV endScreen;
