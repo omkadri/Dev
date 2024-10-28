cbe125a58c4fd46864abcc10000bafcc328a162c
Omar <omkadri@hotmail.com>
2024-10-28 17:33:23 -0400

    Added Animation to flappy bird prototype + Reorganized Asset Folder Names

--- Unity/DPCoreUnity/Assets/R&D/Prototypes/2D Games/FlappyBird2D/FlappyPlayerController2D.cs
+++ Unity/DPCoreUnity/Assets/R&D/Prototypes/2D Games/FlappyBird2D/FlappyPlayerController2D.cs
@@ -2,11 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bird : MonoBehaviour
+public class FlappyPlayerController2D : MonoBehaviour
 {
     public float jumpForce = 5f;
-    public Sprite riseSprite; // Assign this in the Inspector
-    public Sprite fallSprite; // Assign this in the Inspector
     private Rigidbody2D rb;
     private SpriteRenderer spriteRenderer;
 
@@ -21,16 +19,6 @@ public class Bird : MonoBehaviour
         if (Input.GetButtonDown("Jump"))
         {
             rb.velocity = Vector2.up * jumpForce;
-        }
-
-        // Change the sprite based on the bird's vertical velocity
-        if (rb.velocity.y > 0)
-        {
-            spriteRenderer.sprite = riseSprite;
-        }
-        else if (rb.velocity.y < 0)
-        {
-            spriteRenderer.sprite = fallSprite;
         }
     }
 }
