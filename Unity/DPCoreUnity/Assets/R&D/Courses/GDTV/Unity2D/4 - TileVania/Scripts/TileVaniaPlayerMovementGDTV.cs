abc8333baa3a46a5df5e53b1507e06d6e0f00bae
Omar <omkadri@hotmail.com>
2024-10-31 16:06:27 -0400

    TileVania Player now moves left and right

--- Unity/DPCoreUnity/Assets/R&D/Courses/GDTV/Unity2D/4 - TileVania/Scripts/TileVaniaPlayerMovementGDTV.cs
+++ Unity/DPCoreUnity/Assets/R&D/Courses/GDTV/Unity2D/4 - TileVania/Scripts/TileVaniaPlayerMovementGDTV.cs
@@ -7,19 +7,27 @@ using UnityEngine.InputSystem;
 public class TileVaniaPlayerMovementGDTV : MonoBehaviour
 {
     Vector2 moveInput;
+    Rigidbody2D rb2d;
+    [SerializeField] float runSpeed = 10f;
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+        Run();   
     }
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
         Debug.Log(moveInput);
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, rb2d.velocity.y);
+        rb2d.velocity = playerVelocity;
     }
 }
