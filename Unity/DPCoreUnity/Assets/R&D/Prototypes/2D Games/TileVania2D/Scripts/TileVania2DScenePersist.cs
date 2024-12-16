de7035fdf5084fb5016fc7be02ca9df7573d62d4
Omar <omkadri@hotmail.com>
2024-12-15 23:13:05 -0500

    Ported all TileVaniaGDTV files into prototypes folder

---
+++ Unity/DPCoreUnity/Assets/R&D/Prototypes/2D Games/TileVania2D/Scripts/TileVania2DScenePersist.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileVania2DScenePersist : MonoBehaviour
+{
+    void Awake()
+    {
+        int numScenePersists = FindObjectsOfType<TileVania2DScenePersist>().Length;
+        if ( numScenePersists > 1 ) // ensures that only one scenePersist can exist at a time
+        {
+            Destroy( gameObject );
+        }
+        else
+        {
+            DontDestroyOnLoad( gameObject ); //ensures that scenePersist is being carried over into the next scene
+        }
+    }
+
+    public void ResetScenePersist()
+    {
+        Destroy( gameObject ); // we don't want one scenePersist object going into another scene
+    }
+}
