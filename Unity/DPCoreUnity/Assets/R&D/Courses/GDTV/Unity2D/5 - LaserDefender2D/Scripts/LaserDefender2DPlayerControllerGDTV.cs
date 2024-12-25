19e2123afa6691d81ac6fb20d49edf91b9f0a305
Omar <omkadri@hotmail.com>
2024-12-24 19:30:32 -0500

    LaserDfender2D Player no longer leaves camera view
    - Used mathf.Clamp to ensure that player x and y position cannot exceed camera bounds
    - Implemented padding to fine tune how much of the player sprite can still be visible

--- Unity/DPCoreUnity/Assets/R&D/Courses/GDTV/Unity2D/5 - LaserDefender2D/Scripts/LaserDefender2DPlayerControllerGDTV.cs
+++ Unity/DPCoreUnity/Assets/R&D/Courses/GDTV/Unity2D/5 - LaserDefender2D/Scripts/LaserDefender2DPlayerControllerGDTV.cs
@@ -5,17 +5,41 @@ using UnityEngine.InputSystem;
 
 public class LaserDefender2DPlayerControllerGDTV : MonoBehaviour
 {
-    Vector2 rawInput;
     [SerializeField] float moveSpeed = 10f;
+    Vector2 rawInput;
+
+    [SerializeField] float paddingLeft;
+    [SerializeField] float paddingRight;
+    [SerializeField] float paddingTop;
+    [SerializeField] float paddingBottom;
+
+    Vector2 minBounds;
+    Vector2 maxBounds;
+
+    void Start()
+    {
+        InitBounds();
+    }
+
     void Update()
     {
         Move();
     }
 
+    void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint( new Vector2( 0, 0 ) );
+        maxBounds = mainCamera.ViewportToWorldPoint( new Vector2( 1, 1 ) );
+    }
+
     void Move()
     {
-        Vector3 delta = rawInput;
-        transform.position += ((moveSpeed * delta) * Time.deltaTime);
+        Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Clamp( transform.position.x + delta.x, minBounds.x + paddingLeft, maxBounds.x - paddingRight );
+        newPos.y = Mathf.Clamp( transform.position.y + delta.y, minBounds.y + paddingBottom, maxBounds.y - paddingTop );
+        transform.position = newPos;
     }
 
     void OnMove( InputValue value )
