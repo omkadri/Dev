f25d5bb116c2ca862e5107a5a9e11e63cdb5c00e
Omar <omkadri@hotmail.com>
2025-01-12 15:04:03 -0500

    TopDownAction2D: Player sword now orients itself to the mouse and player position
    - Player can now swing sword in opposite direction
    - Created MouseFollowWithOffset() in sword script, which rotates the sword relative to both the mouse and player position.
    - adjusted rotation of sword in scene to look better with new changes.

--- Unity/DPCoreUnity/Assets/R&D/Courses/GDTV/Unity2D/6 - TopDownActionRPG2D/Scripts/GDTVTopDownAction2DSword.cs
+++ Unity/DPCoreUnity/Assets/R&D/Courses/GDTV/Unity2D/6 - TopDownActionRPG2D/Scripts/GDTVTopDownAction2DSword.cs
@@ -4,15 +4,19 @@ public class GDTVTopDownAction2DSword : MonoBehaviour
 {
     GDTVTopDownAction2DInputActions playerInputActions;
     Animator animator; 
+    GDTVTopDownAction2DPlayerController playerController;
+    GDTVTopDownAction2DActiveWeapon activeWeapon;
 
     void Awake()
     {
+        playerController = GetComponentInParent<GDTVTopDownAction2DPlayerController>();
+        activeWeapon = GetComponentInParent<GDTVTopDownAction2DActiveWeapon>();
+        animator = GetComponent<Animator>();
         playerInputActions = new GDTVTopDownAction2DInputActions();
     }
 
     void OnEnable()
     {
-        animator = GetComponent<Animator>();
         playerInputActions.Enable();
     }
 
@@ -21,8 +25,30 @@ public class GDTVTopDownAction2DSword : MonoBehaviour
         playerInputActions.Combat.Attack.started += _ => Attack();
     }
 
+    void Update()
+    {
+        MouseFollowWithOffset();
+    }
+
     void Attack()
     {
         animator.SetTrigger( "Attack" );
+    }
+
+    void MouseFollowWithOffset() //orients the sword relative to the mouse
+    {
+        Vector2 mousePosition = Input.mousePosition;
+        Vector2 playerPosition = Camera.main.WorldToScreenPoint( playerController.transform.position );
+
+        float angle = Mathf.Atan2( mousePosition.y - playerPosition.y, Mathf.Abs( mousePosition.x - playerPosition.x ) ) * Mathf.Rad2Deg;
+        
+        if ( mousePosition.x < playerPosition.x )
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler( 0, -180, angle );
+        }
+        else
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler( 0, 0, angle );
+        }
     }
 }
