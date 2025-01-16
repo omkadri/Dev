369d51a284aa930cabda9b1bad1f0b1b5cecb7a2
Omar <omkadri@hotmail.com>
2025-01-16 15:46:14 -0500

    DPCore: Organized UI prototype folder with some live service staging

---
+++ Unity/DPCoreUnity/Assets/R&D/Prototypes/2D/Driver2D/Scripts/Driver2DPlayerController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver2DPlayerController : MonoBehaviour
+{
+    [SerializeField] float steerSpeed = 900f;
+    [SerializeField] float moveSpeed = 12f;
+    [SerializeField] bool enableBoost = true;
+    float defaultSpeed;
+
+    [SerializeField] float boostSpeed = 20f;
+
+    void Start()
+    {
+        defaultSpeed = moveSpeed;
+    }
+
+    void Update()
+    {
+        float steerAmount = Input.GetAxis("Horizontal");
+        float moveAmount = Input.GetAxis("Vertical");
+        transform.Rotate(0, 0, ( -steerAmount * steerSpeed ) * Time.deltaTime );
+        transform.Translate( 0, ( moveAmount * moveSpeed ) * Time.deltaTime, 0 );
+    }
+    
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if ( other.gameObject.tag != "Boost" )
+        {
+            moveSpeed = defaultSpeed;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if( enableBoost && other.tag == "Boost" )
+        {
+            moveSpeed = boostSpeed;
+        }
+    }
+}
