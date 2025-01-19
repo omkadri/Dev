263a563c19f5a4a9a2e6b8d8b4dce8e703d484cf
Omar <omkadri@hotmail.com>
2025-01-19 15:27:45 -0500

    TopDownAction2D: Created transparency detection script for when player passes under game objects and still needs to be seen.
    - script uses a LERP within a coroutine to gradually set the transparency of an object while the player is passing through it
    - attached script to canopy tilemap and trees (as well as adding trigger colliders)

---
+++ Unity/DPCoreUnity/Assets/R&D/Courses/GDTV/Unity2D/6 - TopDownActionRPG2D/Scripts/Misc/GDTVTopDownAction2DTransparencyDetection.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GDTVTopDownAction2DTransparencyDetection : MonoBehaviour
+{
+    [Range( 0, 1 )]
+    [SerializeField] float transparencyAmount = 0.8f;
+    [SerializeField] float fadeTime = 0.4f;
+
+    SpriteRenderer spriteRenderer;
+    Tilemap tilemap;
+
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        tilemap = GetComponent<Tilemap>();
+    }
+
+
+    void OnTriggerEnter2D( Collider2D other )
+    {
+        if ( other.gameObject.GetComponent<GDTVTopDownAction2DPlayerController>() )
+        {
+            if ( spriteRenderer )
+            {
+                StartCoroutine( FadeSpriteRoutine( spriteRenderer, fadeTime, spriteRenderer.color.a, transparencyAmount ) );
+            }
+            else if ( tilemap )
+            {
+                StartCoroutine( FadeTilemapRoutine( tilemap, fadeTime, tilemap.color.a, transparencyAmount ) );
+            }
+        }
+    }
+
+
+    void OnTriggerExit2D( Collider2D other )
+    {
+        if ( other.gameObject.GetComponent<GDTVTopDownAction2DPlayerController>() )
+        {
+            if ( spriteRenderer )
+            {
+                StartCoroutine( FadeSpriteRoutine( spriteRenderer, fadeTime, spriteRenderer.color.a, 1f ) );
+            }
+            else if ( tilemap )
+            {
+                StartCoroutine( FadeTilemapRoutine( tilemap, fadeTime, tilemap.color.a, 1f ) );
+            }
+        }
+    }
+
+
+    IEnumerator FadeSpriteRoutine( SpriteRenderer spriteRenderer, float fadeTime, float startValue, float targetTransparency )
+    {
+        float elapsedTime = 0;
+        while( elapsedTime <fadeTime )
+        {
+            elapsedTime += Time.deltaTime;
+            float newAlpha = Mathf.Lerp( startValue, targetTransparency, elapsedTime / fadeTime );
+            spriteRenderer.color = new Color( spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, newAlpha );
+            yield return null;
+        }
+    }
+
+
+    IEnumerator FadeTilemapRoutine( Tilemap tilemap, float fadeTime, float startValue, float targetTransparency )
+    {
+        float elapsedTime = 0;
+        while( elapsedTime <fadeTime )
+        {
+            elapsedTime += Time.deltaTime;
+            float newAlpha = Mathf.Lerp( startValue, targetTransparency, elapsedTime / fadeTime );
+            tilemap.color = new Color( tilemap.color.r, tilemap.color.g, tilemap.color.b, newAlpha );
+            yield return null;
+        }
+    }
+}
