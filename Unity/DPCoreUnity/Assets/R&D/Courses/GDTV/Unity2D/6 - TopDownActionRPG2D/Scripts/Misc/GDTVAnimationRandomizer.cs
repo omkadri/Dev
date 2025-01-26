b29ec370e7b7e63a039428d7fb7df1d3994afdcb
Omar <omkadri@hotmail.com>
2025-01-26 14:43:22 -0500

    TopDownAction2D: torch animation instances know have random start times
    - Created AnimationRandomizer script that randomizes changes the starting frame of the animation of the object it is attached to
    - added AnimationRandomizer to flame child of torch

---
+++ Unity/DPCoreUnity/Assets/R&D/Courses/GDTV/Unity2D/6 - TopDownActionRPG2D/Scripts/Misc/GDTVAnimationRandomizer.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GDTVAnimationRandomizer : MonoBehaviour
+{
+    Animator animator;
+
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+
+    void Start()
+    {
+        AnimatorStateInfo state = animator.GetCurrentAnimatorStateInfo( 0 );
+        animator.Play( state.fullPathHash, -1, Random.Range( 0f, 1f ) );
+    }
+}
