f9f93a06208f9106ed21eba5653548e86d0ed53c
Omar <omkadri@hotmail.com>
2025-02-02 12:24:15 -0500

    TopDown2D: Bow now has kickback animation
    - Created kickback animation for bow that triggers every time the bow attacks
    - implemented hashing for animator.SetTrigger call to optimize performance

--- Unity/DPCoreUnity/Assets/R&D/Courses/GDTV/Unity2D/6 - TopDown2D/Scripts/Inventory/GDTVTopDown2DBow.cs
+++ Unity/DPCoreUnity/Assets/R&D/Courses/GDTV/Unity2D/6 - TopDown2D/Scripts/Inventory/GDTVTopDown2DBow.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GDTVTopDown2DBow : MonoBehaviour, GDTVTopDown2DIWeapon
@@ -6,10 +7,21 @@ public class GDTVTopDown2DBow : MonoBehaviour, GDTVTopDown2DIWeapon
     [SerializeField] GameObject arrowPrefab;
     [SerializeField] Transform arrowSpawnPoint;
 
+    Animator animator;
+
+    readonly int FIRE_HASH = Animator.StringToHash( "Fire" );//We convert the string into a hash because strings are more expensive in memory. 
+                                                            //the hash represents the string as an integer, which allows for faster processing
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
 
     public void Attack()
     {
         Debug.Log( "Bow Attack" );
+        animator.SetTrigger( FIRE_HASH );
         GameObject newArrow = Instantiate( arrowPrefab, arrowSpawnPoint.transform.position, GDTVTopDown2DActiveWeapon.Instance.transform.rotation );
     }
 
