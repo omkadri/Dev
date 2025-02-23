92cdf09bdd9d962eff7977b26acef0fc3012fc47
Omar <omkadri@hotmail.com>
2025-02-22 19:18:43 -0500

    TopDown2D: Grape enemy now lobs grape projectile.
    - Created new grape projectile script (which will need to be renamed in the future ). new script contains 2 LERP coroutines
    - the first LERP moves the grape projectile according to the value of a serialized animation curve, which gives it the arch trajectory
    - the second LERP mover a serialized shadow prefab directly towards the player
    - the combination of these 2 LERPs give the projectiles depth and the appearance of being lobbed

---
+++ Unity/DPCoreUnity/Assets/R&D/Courses/GDTV/Unity2D/6 - TopDown2D/Scripts/Enemies/GDTVTopDown2DGrapeProjectile.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using UnityEngine;
+
+public class GDTVTopDown2DGrapeProjectile : MonoBehaviour //TODO: Investigate renaming projectile to something else (throwable, or launchable)
+{
+    [SerializeField] float duration = 1f;
+    [SerializeField] AnimationCurve animCurve;
+    [SerializeField] float heightY = 3f;
+    [SerializeField] GameObject grapeProjectileShadow;
+
+
+    void Start()
+    {
+        GameObject grapeShadow = Instantiate( grapeProjectileShadow, transform.position + new Vector3( 0, -0.3f, 0 ), Quaternion.identity );
+
+        Vector3 playerPos = GDTVTopDown2DPlayerController.Instance.transform.position;
+        Vector3 grapeShadowStarPos = grapeShadow.transform.position;
+
+        StartCoroutine( ProjectileCurveRoutine( transform.position, playerPos ) );
+        StartCoroutine( MoveGrapeShadowRoutine( grapeShadow, grapeShadowStarPos, playerPos ) );
+    }
+
+
+    IEnumerator ProjectileCurveRoutine( Vector3 startPos, Vector3 endPos )
+    {
+        float timePassed = 0f;
+
+        while ( timePassed < duration )
+        {
+            timePassed += Time.deltaTime;
+            float linearT = timePassed / duration;
+            float heightT = animCurve.Evaluate( linearT );
+            float height = Mathf.Lerp( 0f, heightY, heightT );
+
+            transform.position = Vector2.Lerp( startPos, endPos, linearT ) + new Vector2( 0f, height );
+
+            yield return null;
+        }
+
+        Destroy( gameObject );
+    }
+
+
+    IEnumerator MoveGrapeShadowRoutine( GameObject grapeShadow, Vector3 startPos, Vector3 endPos )
+    {
+        float timePassed = 0f;
+
+        while ( timePassed < duration )
+        {
+            timePassed += Time.deltaTime;
+            float linearT = timePassed / duration;
+
+            grapeShadow.transform.position = Vector2.Lerp( startPos, endPos, linearT );
+
+            yield return null;
+        }
+
+        Destroy( grapeShadow );
+    }
+}
