c6ec59b34f2e6f75fd825cf6032cd4f45dd64972
Omar <omkadri@hotmail.com>
2025-03-11 18:19:21 -0400

    TopDown2D: Pickups now pop into a random direction when spawned.
    - Created a coroutine with a LERP (similar to that of the grape projectile). the linearT value comes from a serialized animCurve

--- Unity/DPCoreUnity/Assets/R&D/2D/TopDown2D/Scripts/Misc/TopDown2DPickup.cs
+++ Unity/DPCoreUnity/Assets/R&D/2D/TopDown2D/Scripts/Misc/TopDown2DPickup.cs
@@ -1,10 +1,14 @@
+using System.Collections;
 using UnityEngine;
 
 public class TopDown2DPickup : MonoBehaviour
 {
     [SerializeField] float pickupDistance = 5f;
-    [SerializeField] float accelerationRate = 0.2f;
+    [SerializeField] float accelerationRate = .2f;
     [SerializeField] float moveSpeed = 3f;
+    [SerializeField] AnimationCurve animCurve;
+    [SerializeField] float heightY = 1.5f;
+    [SerializeField] float popDuration = 1f;
 
     Vector3 moveDir;
     Rigidbody2D rb2d;
@@ -14,6 +18,13 @@ public class TopDown2DPickup : MonoBehaviour
     {
         rb2d = GetComponent<Rigidbody2D>();
     }
+
+
+    void Start()
+    {
+        StartCoroutine( AnimCurveSpawnRoutine() );
+    }
+
 
     void Update()
     {
@@ -42,6 +53,28 @@ public class TopDown2DPickup : MonoBehaviour
         if ( other.gameObject.GetComponent<TopDown2DPlayerController>() )
         {
             Destroy( gameObject );
+        }
+    }
+
+
+    IEnumerator AnimCurveSpawnRoutine()
+    {
+        Vector2 startPos = transform.position;
+        float randomX = transform.position.x + Random.Range( -2f, 2f);
+        float randomY = transform.position.y + Random.Range( -1f, 1f);
+        Vector2 endPos = new Vector2( randomX, randomY );
+        float timePassed = 0f;
+
+        while ( timePassed < popDuration )
+        {
+            timePassed += Time.deltaTime;
+            float linearT = timePassed / popDuration;
+            float heightT = animCurve.Evaluate( linearT );
+            float height = Mathf.Lerp( 0f, heightY, heightT );
+
+            transform.position = Vector2.Lerp( startPos, endPos, linearT ) + new Vector2( 0f, height );
+
+            yield return null;
         }
     }
 }
--- Unity/DPCoreUnity/Assets/R&D/2D/TopDown2D/Scripts/Misc/TopDown2DPickupSpawner.cs
+++ Unity/DPCoreUnity/Assets/R&D/2D/TopDown2D/Scripts/Misc/TopDown2DPickupSpawner.cs
@@ -4,6 +4,7 @@ public class TopDown2DPickupSpawner : MonoBehaviour
 {
     [SerializeField] GameObject pickupPrefab;
 
+
     public void DropItems()
     {
         Instantiate( pickupPrefab, transform.position, Quaternion.identity );
