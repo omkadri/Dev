a4003f2a3386361f878dade68d24b5652f644491
Omar <omkadri@hotmail.com>
2025-03-12 17:24:49 -0400

    TopDown2D: Implemented cursor

---
+++ Unity/DPCoreUnity/Assets/R&D/2D/TopDown2D/Scripts/Misc/TopDown2DCursor.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TopDown2DCursor : MonoBehaviour
+{
+
+    Image cursorImage;
+
+
+    void Awake()
+    {
+        cursorImage = GetComponent<Image>();
+    }
+
+
+    void Start()
+    {
+        Cursor.visible = false;
+        if ( Application.isPlaying )
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+    }
+
+
+    void Update()
+    {
+        Vector2 cursorPos = Input.mousePosition;
+        cursorImage.rectTransform.position = cursorPos;
+        transform.position = cursorPos;
+
+        //if ( !Application.isPlaying ) { return; }
+
+        //Cursor.visible = false;
+    }
+}
