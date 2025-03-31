69a8e52d2bc8db72fb1b703f5fdf8e8a36b7b725
Omar <omkadri@hotmail.com>
2025-03-31 11:37:30 -0400

    ShootEmUp2D: Created Bounce Puff VFX
    - Created Poof Dust Particle Material
    - Applied that materials to several prototype particle systems (boiling, Waterfall, Jump Puff)
    - Added BouncePuff particle system to particles child of player object.
    - BouncePuff particles now trigger when the player jumps

--- Unity/GameFeelCourse/Assets/ShootEmUp2D/Scripts/Player/ShootEmUp2DPlayerAnimations.cs
+++ Unity/GameFeelCourse/Assets/ShootEmUp2D/Scripts/Player/ShootEmUp2DPlayerAnimations.cs
@@ -9,14 +9,27 @@ public class ShootEmUp2DPlayerAnimations : MonoBehaviour
     [SerializeField] float _playerHatTiltAngle = 14f;
     [SerializeField] float _playerHatTiltSpeed = 5f;
     [SerializeField] Transform _playerHatSpriteTransform;
+    [SerializeField] ParticleSystem bouncePuffVFX;
 
 
-        void Update()
-        {
-            DetectPlayerMoveDust();
-            ApplyPlayerBodyTilt();
-            ApplyPlayerHatTilt();
-        }
+    void Update()
+    {
+        DetectPlayerMoveDust();
+        ApplyPlayerBodyTilt();
+        ApplyPlayerHatTilt();
+    }
+
+
+    void OnEnable()
+    {
+        ShootEmUp2DPlayerController.OnJump += PlayBouncePuffParticle;
+    }
+
+
+    void OnDisable()
+    {
+        ShootEmUp2DPlayerController.OnJump -= PlayBouncePuffParticle;
+    }
 
 
         void DetectPlayerMoveDust()
@@ -38,50 +51,56 @@ public class ShootEmUp2DPlayerAnimations : MonoBehaviour
         }
 
 
-        void ApplyPlayerBodyTilt()
+    void ApplyPlayerBodyTilt()
+    {
+        float targetAngle;
+        if( ShootEmUp2DPlayerController.Instance.MoveInput.x < 0f )
+        {
+            targetAngle = _playerBodyTiltAngle;//TODO: Implement logic that makes player tilting consistent when wacing left of right
+        }
+        else if( ShootEmUp2DPlayerController.Instance.MoveInput.x > 0f )
+        {
+            targetAngle = -_playerBodyTiltAngle;
+        }
+        else
+        {
+            targetAngle = 0f;
+        }
+
+        Quaternion currentPlayerRotation = _playerBodySpriteTransform.rotation;//TODO: Understand Quaternions and Eulers
+        Quaternion targetPlayerRotation = Quaternion.Euler( currentPlayerRotation.eulerAngles.x, currentPlayerRotation.eulerAngles.y, targetAngle );
+
+        _playerBodySpriteTransform.rotation = Quaternion.Lerp( currentPlayerRotation, targetPlayerRotation, _playerBodyTiltSpeed * Time.deltaTime );
+    }
+
+
+    void ApplyPlayerHatTilt()
+    {
         {
             float targetAngle;
-            if( ShootEmUp2DPlayerController.Instance.MoveInput.x < 0f )
+            if( ShootEmUp2DPlayerController.Instance.MoveInput.x > 0f )
             {
-                targetAngle = _playerBodyTiltAngle;//TODO: Implement logic that makes player tilting consistent when wacing left of right
+                targetAngle = _playerHatTiltAngle;//TODO: Implement logic that makes player tilting consistent when wacing left of right
             }
-            else if( ShootEmUp2DPlayerController.Instance.MoveInput.x > 0f )
+            else if( ShootEmUp2DPlayerController.Instance.MoveInput.x < 0f )
             {
-                targetAngle = -_playerBodyTiltAngle;
+                targetAngle = -_playerHatTiltAngle;
             }
             else
             {
                 targetAngle = 0f;
             }
 
-            Quaternion currentPlayerRotation = _playerBodySpriteTransform.rotation;//TODO: Understand Quaternions and Eulers
-            Quaternion targetPlayerRotation = Quaternion.Euler( currentPlayerRotation.eulerAngles.x, currentPlayerRotation.eulerAngles.y, targetAngle );
+            Quaternion currentHatRotation = _playerHatSpriteTransform.rotation;//TODO: Understand Quaternions and Eulers
+            Quaternion targetHatRotation = Quaternion.Euler( currentHatRotation.eulerAngles.x, currentHatRotation.eulerAngles.y, targetAngle );
 
-            _playerBodySpriteTransform.rotation = Quaternion.Lerp( currentPlayerRotation, targetPlayerRotation, _playerBodyTiltSpeed * Time.deltaTime );
+            _playerHatSpriteTransform.rotation = Quaternion.Lerp( currentHatRotation, targetHatRotation, _playerHatTiltSpeed * Time.deltaTime );
         }
+    }
 
 
-        void ApplyPlayerHatTilt()
-        {
-            {
-                float targetAngle;
-                if( ShootEmUp2DPlayerController.Instance.MoveInput.x > 0f )
-                {
-                    targetAngle = _playerHatTiltAngle;//TODO: Implement logic that makes player tilting consistent when wacing left of right
-                }
-                else if( ShootEmUp2DPlayerController.Instance.MoveInput.x < 0f )
-                {
-                    targetAngle = -_playerHatTiltAngle;
-                }
-                else
-                {
-                    targetAngle = 0f;
-                }
-
-                Quaternion currentHatRotation = _playerHatSpriteTransform.rotation;//TODO: Understand Quaternions and Eulers
-                Quaternion targetHatRotation = Quaternion.Euler( currentHatRotation.eulerAngles.x, currentHatRotation.eulerAngles.y, targetAngle );
-
-                _playerHatSpriteTransform.rotation = Quaternion.Lerp( currentHatRotation, targetHatRotation, _playerHatTiltSpeed * Time.deltaTime );
-            }
-        }
+    void PlayBouncePuffParticle()
+    {
+        bouncePuffVFX.Play();
+    }
 }
