f77e7d46a9b0c17581f4f3bd438eb43c04fe9067
Omar <omkadri@hotmail.com>
2025-04-06 12:26:32 -0400

    ShootEmUp: Implemented support for sound pitch randomization
    - Adjusted PlaySound() function to require parameters for volume, pitch and looping.
    - Created PlayProcessedSound() function, which retrieves and processes volume, pitch and looping. This also calls PlaySound with the processed parameters.
    - pit is randomized via randomizePitchModifier
    - PlayProcessedSound only requires the clip as a parameter, and is now used where PlaySound() used to be called.
    - applied pitch randomization to shoot and jump sound

--- Unity/DPCoreUnity/Assets/Scripts/ShootEmUp2D/Audio/ShootEmUp2DAudioManager.cs
+++ Unity/DPCoreUnity/Assets/Scripts/ShootEmUp2D/Audio/ShootEmUp2DAudioManager.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 
 public class ShootEmUp2DAudioManager : MonoBehaviour
 {
-    [SerializeField] ShootEmUp2DSoundSO _rangedWeaponShoot;//TODO: find better name
-    [SerializeField] ShootEmUp2DSoundSO _playerJump;//TODO: find better name
+    [SerializeField] float _masterVolume = 1f;
+    [SerializeField] ShootEmUp2DSoundSO _playerRangedWeaponShootSFX;//TODO: find better name
+    [SerializeField] ShootEmUp2DSoundSO _playerJumpSFX;//TODO: find better name
 
     void OnEnable()
     {
@@ -19,23 +20,50 @@ public class ShootEmUp2DAudioManager : MonoBehaviour
     }
 
 
-    void PlaySound( ShootEmUp2DSoundSO soundSO)
+    void PlayProcessedSound( ShootEmUp2DSoundSO soundSO )
+    {
+        AudioClip clip = soundSO.Clip;
+        float volume = soundSO.Volume * _masterVolume;
+        float pitch = soundSO.Pitch;
+        bool loop = soundSO.Loop;
+        //TODO: add playback speed modifier (0.5x, 2x, etc)
+        //TODO: add playback speed randomization.
+
+        if( soundSO.RandomizePitch )
+        {
+            float randomizePitchModifier = Random.Range( -soundSO.RandomPitchRangeModifier, soundSO.RandomPitchRangeModifier );
+            pitch = soundSO.Pitch + randomizePitchModifier;
+        }
+
+        PlaySound( clip, volume, pitch, loop );
+    }
+
+
+    void PlaySound( AudioClip clip, float volume, float pitch, bool loop )
     {
         GameObject soundObject = new GameObject( "Temp Audio Source" );
         AudioSource audioSource = soundObject.AddComponent<AudioSource>();
-        audioSource.clip = soundSO.Clip;
+        audioSource.clip = clip;
+        audioSource.volume = volume;
+        audioSource.pitch = pitch;
+        audioSource.loop = loop;
         audioSource.Play();
+        
+        if ( !loop )
+        {
+            Destroy( soundObject, clip.length );//TODO: implement object pooling
+        }
     }
 
 
     void RangedWeapon_OnShoot()//TODO: find better naming convention
     {
-        PlaySound( _rangedWeaponShoot );
+        PlayProcessedSound( _playerRangedWeaponShootSFX );
     }
 
 
     void PlayerController_OnJump()//TODO: find better naming convention
     {
-        PlaySound( _playerJump );
+        PlayProcessedSound( _playerJumpSFX );
     }
 }
