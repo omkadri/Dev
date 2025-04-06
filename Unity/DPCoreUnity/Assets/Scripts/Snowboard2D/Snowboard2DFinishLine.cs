244a58454a7f7256574e57ddc3866fcf05303acf
Omar <omkadri@hotmail.com>
2025-04-05 20:04:12 -0400

    DPCore: Updated Reference folder

---
+++ Unity/DPCoreUnity/Assets/Scripts/Snowboard2D/Snowboard2DFinishLine.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Snowboard2DFinishLine : MonoBehaviour
+{
+  [SerializeField] float _loadDelay = 1.5f;
+  [SerializeField] ParticleSystem _finishParticleA;
+  [SerializeField] ParticleSystem _finishParticleB;
+  [SerializeField] AudioClip _finishSFX;
+  
+
+  void OnTriggerEnter2D(Collider2D other)
+  {
+    if ( other.tag == "Player" )
+    {
+      //Create OnComplete callback
+        Debug.Log( "You Finished!" );
+        _finishParticleA.Play();
+        _finishParticleB.Play();
+        if ( _finishSFX != null )
+        {
+          GetComponent<AudioSource>().PlayOneShot(_finishSFX);
+        }
+        Invoke( "ReloadScene", _loadDelay );
+    }
+  }
+
+
+  void ReloadScene()
+  {
+    SceneManager.LoadScene( "Snowboard2DBaseScene" );
+  }
+}
