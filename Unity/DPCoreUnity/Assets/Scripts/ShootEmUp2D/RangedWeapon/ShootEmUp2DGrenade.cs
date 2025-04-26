1cbab1729d64bae0904d413d1bf5f4b3eea20061
Omar <omkadri@hotmail.com>
2025-04-26 18:02:33 -0400

    ShootEmUp2D: Grenade now launches and explodes when colliding with an enemy.
    - Created LaunchGrenade() function, which adds directional force and torque to the grenade as soon as it is instantiated (relative to mouse position)
    - Created OnGrenadeExplode event, which currently triggers when colliding with an enemy.
    - Created and subscribed GrenadeExplosion() and GrenadeScreenShake() functions to the OnGrenadeExplode event.
    - turned on enemies for testing purposes
    -

--- Unity/DPCoreUnity/Assets/Scripts/ShootEmUp2D/RangedWeapon/ShootEmUp2DGrenade.cs
+++ Unity/DPCoreUnity/Assets/Scripts/ShootEmUp2D/RangedWeapon/ShootEmUp2DGrenade.cs
@@ -1,16 +1,73 @@
 using UnityEngine;
+using Unity.Cinemachine;
+using System;
 
 public class ShootEmUp2DGrenade : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Action OnGrenadeExplode; //we don't want a static action because there will be many instances of grenades. 
+
+    [SerializeField] GameObject _explodeVFX;
+    [SerializeField] float _launchForce = 15f;
+    [SerializeField] float _torqueAmount = 0.5f;
+
+    Rigidbody2D _rb2d;
+    CinemachineImpulseSource _impulseSource;
+
+
+    void OnEnable()
     {
-        
+        OnGrenadeExplode += GrenadeExplosion;
+        OnGrenadeExplode += GrenadeScreenShake;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void OnDisable()
     {
-        
+        OnGrenadeExplode -= GrenadeExplosion;
+        OnGrenadeExplode -= GrenadeScreenShake;
+    }
+
+
+    void Awake()
+    {
+        _rb2d = GetComponent<Rigidbody2D>();
+        _impulseSource = GetComponent<CinemachineImpulseSource>();
+    }
+
+
+    void Start()
+    {
+        LaunchGrenade();
+    }
+
+
+    void OnCollisionEnter2D( Collision2D other )
+    {
+        if( other.gameObject.GetComponent<ShootEmUp2DEnemy>() )
+        {
+            OnGrenadeExplode?.Invoke();
+        }
+    }
+
+
+    void LaunchGrenade()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint( Input.mousePosition ); //TODO: Investigate exactly what ScreenToWorldPoint does
+        Vector2 dirToMouse = ( mousePos - (Vector2)transform.position ).normalized; //TODO: investigate vector2 casting and normalization
+        _rb2d.AddForce( dirToMouse * _launchForce, ForceMode2D.Impulse );
+        _rb2d.AddTorque( _torqueAmount, ForceMode2D.Impulse );
+    }
+
+
+    void GrenadeExplosion()
+    {
+        Instantiate( _explodeVFX, transform.position, Quaternion.identity );
+        Destroy( gameObject );
+    }
+
+
+    void GrenadeScreenShake()
+    {
+        _impulseSource.GenerateImpulse();
     }
 }
