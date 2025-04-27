766945a1962cb50e4b10cfdebdd1bcc77234c501
Omar <omkadri@hotmail.com>
2025-04-27 00:27:09 -0400

    ShootEmUp2D: Grenade now explodes on a timer and can kill enemies
    - Created new enemy layer. Set slime enemies to new enemy layer.
    - Created OnBeep event to trigger functions related to grenade LED light
    - Created DamageEnemiesInRadius() in grenade class. this function checks the enemy layer for collision within the explosion radius, and damages any enemies it finds.
    - Created BlinkLight function, which toggles on the grenade LED light and increments the total blinks.
    - Created OnBeep event that triggers logic for toggling the grenade beeping.
    - Created CountdownExplodeRoutine() which runs on instantiation. This coroutine uses basic algebra to sync up the total blinks with the explosion time. Once that synchronization is finished and the countdown is over, OnGrenadeExplode will be invoked and the grenade will explode.

--- Unity/DPCoreUnity/Assets/Scripts/ShootEmUp2D/RangedWeapon/ShootEmUp2DGrenade.cs
+++ Unity/DPCoreUnity/Assets/Scripts/ShootEmUp2D/RangedWeapon/ShootEmUp2DGrenade.cs
@@ -1,15 +1,25 @@
 using UnityEngine;
 using Unity.Cinemachine;
 using System;
+using System.Collections;
 
 public class ShootEmUp2DGrenade : MonoBehaviour
 {
     public Action OnGrenadeExplode; //we don't want a static action because there will be many instances of grenades. 
+    public Action OnBeep;
 
     [SerializeField] GameObject _explodeVFX;
     [SerializeField] float _launchForce = 15f;
     [SerializeField] float _torqueAmount = 0.5f;
+    [SerializeField] float _explosionRadius = 3.5f;
+    [SerializeField] LayerMask _enemyLayerMask;
+    [SerializeField] int _damageAmount = 3;
+    [SerializeField] GameObject _grenadeLight;
+    [SerializeField] float _lightBlinkTime = 0.15f; //TODO: better name???
+    [SerializeField] int _totalBlinks = 3;
+    [SerializeField] float _explodeTime = 3f;
 
+    int _currentBlinks;
     Rigidbody2D _rb2d;
     CinemachineImpulseSource _impulseSource;
 
@@ -18,6 +28,8 @@ public class ShootEmUp2DGrenade : MonoBehaviour
     {
         OnGrenadeExplode += GrenadeExplosion;
         OnGrenadeExplode += GrenadeScreenShake;
+        OnGrenadeExplode += DamageEnemiesInRadius;
+        OnBeep += BlinkLight;
     }
 
 
@@ -25,6 +37,8 @@ public class ShootEmUp2DGrenade : MonoBehaviour
     {
         OnGrenadeExplode -= GrenadeExplosion;
         OnGrenadeExplode -= GrenadeScreenShake;
+        OnGrenadeExplode -= DamageEnemiesInRadius;
+        OnBeep -= BlinkLight;
     }
 
 
@@ -38,6 +52,7 @@ public class ShootEmUp2DGrenade : MonoBehaviour
     void Start()
     {
         LaunchGrenade();
+        StartCoroutine( CountdownExplodeRoutine() );
     }
 
 
@@ -69,5 +84,38 @@ public class ShootEmUp2DGrenade : MonoBehaviour
     void GrenadeScreenShake()
     {
         _impulseSource.GenerateImpulse();
+    }
+
+
+    void DamageEnemiesInRadius() //TODO: think of better name
+    {
+        Collider2D[] hits = Physics2D.OverlapCircleAll( transform.position, _explosionRadius, _enemyLayerMask );
+        foreach ( Collider2D hit in hits )
+        {
+            ShootEmUp2DHealth health = hit.GetComponent<ShootEmUp2DHealth>();
+            health?.TakeDamage( _damageAmount );
+        }
+    }
+
+
+    IEnumerator CountdownExplodeRoutine()
+    {
+        _grenadeLight.SetActive( false );
+        while ( _currentBlinks < _totalBlinks )
+        {
+            yield return new WaitForSeconds( _explodeTime / _totalBlinks ); //using basic algebra to sync up the beeps with the time
+            OnBeep?.Invoke();
+            yield return new WaitForSeconds( _lightBlinkTime );//TODO: Investigate issue where lowering this value shortens the explode time of the grenade????
+            _grenadeLight.SetActive( false );
+        }
+
+        OnGrenadeExplode?.Invoke();
+    }
+
+
+    void BlinkLight()
+    {
+        _grenadeLight.SetActive( true );
+        _currentBlinks++;
     }
 }
