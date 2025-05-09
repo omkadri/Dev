145b8b230d762b7dc2be3c3a1c93881f93ecde70
Omar <omkadri@hotmail.com>
2025-05-08 20:05:58 -0400

    GlitchGarden2D: Trophy now adds stars to the star display
    - created public function in defender class titled AddStars()
    - Added fading star icon to trophy animation
    - Added animation event to trophy that updates the star display
    - Added star icon beside star counter

--- Unity/DPCoreUnity/Assets/Scripts/GlitchGarden2D/GlitchGarden2DDefender.cs
+++ Unity/DPCoreUnity/Assets/Scripts/GlitchGarden2D/GlitchGarden2DDefender.cs
@@ -4,4 +4,10 @@ using UnityEngine;
 
 public class GlitchGarden2DDefender : MonoBehaviour
 {
+        [SerializeField] int _starCost = 100;
+
+        public void AddStars( int amount )
+        {
+              FindFirstObjectByType<GlitchGarden2DStarDisplay>().AddStars( amount );//TODO: Fix tight coupling
+        }
 }
