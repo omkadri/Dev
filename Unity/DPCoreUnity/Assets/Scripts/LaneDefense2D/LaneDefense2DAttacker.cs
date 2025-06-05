e32e6636da3fee0cb768bd57deff39c0a7684fc7
Omar <omkadri@hotmail.com>
2025-06-05 16:03:22 -0400

    LaneDefense2D: Lizard now plays attacking animation when colliding with a defender.

--- Unity/DPCoreUnity/Assets/Scripts/LaneDefense2D/LaneDefense2DAttacker.cs
+++ Unity/DPCoreUnity/Assets/Scripts/LaneDefense2D/LaneDefense2DAttacker.cs
@@ -4,18 +4,34 @@ using UnityEngine;
 
 public class LaneDefense2DAttacker : MonoBehaviour
 {
-    [Range ( 0f, 5f )][SerializeField] float _currentSpeed = 0f;
+    [Range(0f, 5f)]
+    [SerializeField] float _currentSpeed = 0f;
+    GameObject _currentTarget;
+    Animator _animator;
+    static readonly int _isAttackingHash = Animator.StringToHash( "IsAttacking" );
+
+
+    void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
 
 
     void Update()
     {
-        transform.Translate( Vector2.left* _currentSpeed * Time.deltaTime );
+        transform.Translate(Vector2.left * _currentSpeed * Time.deltaTime);
     }
 
 
-    public void SetMovementSpeed( float speed ) //this is referred to as passing in
-    //This function is being called by the animation event in the unity editor. SPELLING MATTERS!!!!!
+    public void SetMovementSpeed(float speed)
     {
         _currentSpeed = speed;
+    }
+
+
+    public void Attack( GameObject target )
+    {
+        _animator.SetBool(_isAttackingHash, true);
+        _currentTarget = target;
     }
 }
