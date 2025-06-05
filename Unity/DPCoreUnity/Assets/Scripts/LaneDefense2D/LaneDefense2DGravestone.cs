0d2c10d3980b908ea5ad9c4266f2c36bf1d51e9b
Omar <omkadri@hotmail.com>
2025-06-05 18:13:11 -0400

    LaneDefense2D: Created Fox attacker that can jump over gravestones

---
+++ Unity/DPCoreUnity/Assets/Scripts/LaneDefense2D/LaneDefense2DGravestone.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class LaneDefense2DGravestone : MonoBehaviour
+{
+    void OnTriggerStay2D( Collider2D other )
+    {
+        if ( other.GetComponent<LaneDefense2DAttacker>() )
+        {
+            //animate
+        }
+    }
+}
