0bf7f5d1e551bf562e3ea10e5843e9053a462104
Omar <omkadri@hotmail.com>
2025-07-02 19:00:58 -0400

    LaneDefense2D: Fixing issue where players enemies are not getting destroyed after reaching the damage done

--- Unity/DPCoreUnity/Assets/Scripts/LaneDefense2D/LaneDefense2DDamageZone.cs
+++ Unity/DPCoreUnity/Assets/Scripts/LaneDefense2D/LaneDefense2DDamageZone.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 
 public class LaneDefense2DDamageZone : MonoBehaviour
 {
-    void OnTriggerEnter2D()
+    void OnTriggerEnter2D(Collider2D other)
     {
         FindFirstObjectByType<LaneDefense2DLifeDisplay>().TakeLife();
+        Destroy(other.gameObject);
     }
 }
