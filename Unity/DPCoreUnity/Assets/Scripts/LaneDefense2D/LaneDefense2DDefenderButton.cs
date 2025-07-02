97e3f9ad30037b311c581d569fafe87fb66fc5b9
Omar <omkadri@hotmail.com>
2025-07-02 19:28:56 -0400

    LaneDefense2D: Defender buttons now display the cost of the defender

--- Unity/DPCoreUnity/Assets/Scripts/LaneDefense2D/LaneDefense2DDefenderButton.cs
+++ Unity/DPCoreUnity/Assets/Scripts/LaneDefense2D/LaneDefense2DDefenderButton.cs
@@ -1,16 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using TMPro;
 
 public class LaneDefense2DDefenderButton : MonoBehaviour
 {
     [SerializeField] LaneDefense2DDefender _defenderPrefab;
 
 
+    void Start()
+    {
+        LabelButtonWithCost();
+    }
+
+
+    void LabelButtonWithCost()
+    {
+        TMP_Text costText = GetComponentInChildren<TMP_Text>();
+        if (!costText)
+        {
+            Debug.LogError(name + " has no cost text.");
+        }
+        else
+        {
+            costText.text = _defenderPrefab.GetStarCost().ToString();
+        }
+    }
+
+
     private void OnMouseDown()
     {
         LaneDefense2DDefenderButton[] buttons = FindObjectsByType<LaneDefense2DDefenderButton>(FindObjectsSortMode.None); // finds all objects with DefenderButton script attached to them
-        foreach(LaneDefense2DDefenderButton button in buttons)
+        foreach (LaneDefense2DDefenderButton button in buttons)
         {
             button.GetComponent<SpriteRenderer>().color = new Color32(41, 41, 41, 255); //greys out all DefenderButtons
             FindFirstObjectByType<LaneDefense2DDefenderSpawner>().SetSelectedDefender(_defenderPrefab); //passes currently clicked defender into DefenderSpawner
