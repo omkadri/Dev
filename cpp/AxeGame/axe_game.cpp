6ac2d9c82129e9606330ce656e2ce9174e81aeec
Omar <omkadri@hotmail.com>
2025-07-02 20:17:10 -0400

    Raylib: Created a red circle that can be moved left and right

--- cpp/AxeGame/axe_game.cpp
+++ cpp/AxeGame/axe_game.cpp
@@ -1,14 +1,33 @@
 #include "raylib.h"
 int main()
 {
-        int width = 800;
-        int height = 400;
-        InitWindow(width, height, "Axe Game");
+        //window dimensions
+        int _screenWidth{800};
+        int _ScreenHeight{400};
+
+        //Circle Coordinates
+        int circle_x{400};
+        int circle_y{200};
+
+        SetTargetFPS(60);
+        InitWindow(_screenWidth, _ScreenHeight, "Axe Game");
         
         while(WindowShouldClose() == false)
         {
                 BeginDrawing();
                 ClearBackground(WHITE);
+                DrawCircle(circle_x, circle_y, 25, RED);
+
+                if (IsKeyDown(KEY_D) && circle_x <= (_screenWidth - 50))
+                {
+                        circle_x = circle_x + 10;
+                }
+
+                if (IsKeyDown(KEY_A) && circle_x >= 50)
+                {
+                        circle_x = circle_x - 10;
+                }
+
                 EndDrawing();
         }
 }
