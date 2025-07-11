1483e1fd24d4d5dd7de29b122a7abb61e5166955
Omar <omkadri@hotmail.com>
2025-07-10 22:00:34 -0400

    Raylib: cleaned up code for maze game. added goal object that wins the game if player collides with it.

--- Raylib/axe-game/axe_game.cpp
+++ Raylib/axe-game/axe_game.cpp
@@ -11,7 +11,7 @@ int main()
         int _player_y{200};
         int _player_radius{25};
 
-        // player edges
+        //player edges
         int _player_left{_player_x - _player_radius};
         int _player_right{_player_x + _player_radius};
         int _player_top{_player_y - _player_radius};
@@ -28,13 +28,31 @@ int main()
         int _enemy_top{_enemy_y};
         int _enemy_bottom{_enemy_y + _enemy_length};
 
-        int direction = 10;
+        //goal coordinates
+        int _goal_x{700};
+        int _goal_y{200};
+        int _goal_length{50};
+        
+        // goal edges
+        int _goal_left{_goal_x};
+        int _goal_right{_goal_x + _goal_length};
+        int _goal_top{_goal_y};
+        int _goal_bottom{_goal_y + _goal_length};
+
+        int _player_move_speed = 10;
+        int _enemy_move_speed = 10;
 
         bool _collision_with_enemy = 
                                 _enemy_bottom >= _player_top && 
                                 _enemy_top <= _player_bottom && 
                                 _enemy_left <= _player_right && 
                                 _enemy_right >= _player_left;
+        
+        bool _collision_with_goal = 
+                                _goal_bottom >= _player_top && 
+                                _goal_top <= _player_bottom && 
+                                _goal_left <= _player_right && 
+                                _goal_right >= _player_left;
 
         SetTargetFPS(60);
         InitWindow(_screenWidth, _ScreenHeight, "Maze Game");
@@ -48,46 +66,68 @@ int main()
                 {
                         DrawText("Game Over", 400, 200, 20, RED);
                 }
+                else if (_collision_with_goal)
+                {
+                        DrawText("You Win", 400, 200, 20, GREEN);
+                }
                 else
                 {
                         //Game Logic Begins
-                        DrawCircle(_player_x, _player_y, _player_radius, RED);
-                        DrawRectangle(_enemy_x, _enemy_y, _enemy_length, _enemy_length, BLUE);
+                        DrawCircle(_player_x, _player_y, _player_radius, BLUE);
+                        DrawRectangle(_enemy_x, _enemy_y, _enemy_length, _enemy_length, RED);
+                        DrawRectangle(_goal_x, _goal_y, _goal_length, _goal_length, GREEN);
 
-                        //update edges
+                        //update player edges
                         _player_left = _player_x - _player_radius;
                         _player_right = _player_x + _player_radius;
                         _player_top = _player_y - _player_radius;
                         _player_bottom = _player_y + _player_radius;
 
+                        //update enemy edges
                         _enemy_left = _enemy_x;
                         _enemy_right = _enemy_x + _enemy_length;
                         _enemy_top = _enemy_y;
                         _enemy_bottom = _enemy_y + _enemy_length;
 
-                        //update _collision_with_enemy
+                        //update enemy collision
                         _collision_with_enemy = 
                                 _enemy_bottom >= _player_top && 
                                 _enemy_top <= _player_bottom && 
                                 _enemy_left <= _player_right && 
                                 _enemy_right >= _player_left;
 
+                        //update goal collision
+                        _collision_with_goal = 
+                                _goal_bottom >= _player_top && 
+                                _goal_top <= _player_bottom && 
+                                _goal_left <= _player_right && 
+                                _goal_right >= _player_left;
+
                         //move the player
-                        _enemy_y += direction;
+                        _enemy_y += _enemy_move_speed;
 
                         if (_enemy_y > _ScreenHeight || _enemy_y < 0 )
                         {
-                                direction = -direction;
+                                _enemy_move_speed = -_enemy_move_speed;
                         }
 
                         if (IsKeyDown(KEY_D) && _player_x < (_screenWidth - 50))
                         {
-                                _player_x += 10;
+                                _player_x += _player_move_speed;
                         }
 
                         if (IsKeyDown(KEY_A) && _player_x >= 50)
                         {
-                                _player_x -= 10;
+                                _player_x -= _player_move_speed;
+                        }
+                        if (IsKeyDown(KEY_S) && _player_y < (_screenWidth - 50))
+                        {
+                                _player_y += _player_move_speed;
+                        }
+
+                        if (IsKeyDown(KEY_W) && _player_y >= 50)
+                        {
+                                _player_y -= _player_move_speed;
                         }
                 }
                 EndDrawing();
