cb057b2d4b54c5c2a371064bd6552c14cfe87935
Omar <omkadri@hotmail.com>
2025-07-10 21:05:20 -0400

    Raylib: Added smarter collision detection for axe

--- Raylib/AxeGame/axe_game.cpp
+++ Raylib/AxeGame/axe_game.cpp
@@ -8,12 +8,24 @@ int main()
         int _ScreenHeight{450};
 
         //Circle Coordinates
-        int circle_x{200};
-        int circle_y{200};
+        int _circle_x{200};
+        int _circle_y{200};
+        int _circle_radius{25};
+        // circle edges
+        int _left_circle_x{_circle_x - _circle_radius};
+        int _right_circle_x{_circle_x + _circle_radius};
+        int _up_circle_y{_circle_y - _circle_radius};
+        int _bottom_circle_y{_circle_y + _circle_radius};
         
         //Axe Coordinates
-        int axe_x{400};
-        int axe_y{0};
+        int _axe_x{400};
+        int _axe_y{0};
+        int _axe_length{50};
+        // axe edges
+        int _left_axe_x{_axe_x};
+        int _right_axe_x{_axe_x + _axe_length};
+        int _up_axe_y{_axe_y};
+        int _bottom_axe_y{_axe_y + _axe_length};
 
         SetTargetFPS(60);
         InitWindow(_screenWidth, _ScreenHeight, "Axe Game");
@@ -24,25 +36,25 @@ int main()
                 ClearBackground(WHITE);
 
                 //Game Logic Begins
-                DrawCircle(circle_x, circle_y, 25, RED);
-                DrawRectangle(axe_x, axe_y, 50, 50, BLUE);
+                DrawCircle(_circle_x, _circle_y, _circle_radius, RED);
+                DrawRectangle(_axe_x, _axe_y, _axe_length, _axe_length, BLUE);
 
                 //move the axe
-                axe_y += direction;
+                _axe_y += direction;
 
-                if (axe_y > 450 || axe_y < 0 )
+                if (_axe_y > _ScreenHeight || _axe_y < 0 )
                 {
                         direction = -direction;
                 }
 
-                if (IsKeyDown(KEY_D) && circle_x <= (_screenWidth - 50))
+                if (IsKeyDown(KEY_D) && _circle_x < (_screenWidth - 50))
                 {
-                        circle_x += 10;
+                        _circle_x += 10;
                 }
 
-                if (IsKeyDown(KEY_A) && circle_x >= 50)
+                if (IsKeyDown(KEY_A) && _circle_x >= 50)
                 {
-                        circle_x -= 10;
+                        _circle_x -= 10;
                 }
 
                 EndDrawing();
