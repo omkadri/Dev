f2ee1d0a46b732b153b9ec47f20763c30cfe923b
Omar <omkadri@hotmail.com>
2025-07-10 20:54:50 -0400

    Raylib: Renaming cpp folder to Raylib + adding up and down automatic movement to axe

--- Raylib/AxeGame/axe_game.cpp
+++ Raylib/AxeGame/axe_game.cpp
@@ -1,13 +1,19 @@
 #include "raylib.h"
 int main()
 {
+        int direction = 10;
+
         //window dimensions
         int _screenWidth{800};
-        int _ScreenHeight{400};
+        int _ScreenHeight{450};
 
         //Circle Coordinates
-        int circle_x{400};
+        int circle_x{200};
         int circle_y{200};
+        
+        //Axe Coordinates
+        int axe_x{400};
+        int axe_y{0};
 
         SetTargetFPS(60);
         InitWindow(_screenWidth, _ScreenHeight, "Axe Game");
@@ -16,16 +22,27 @@ int main()
         {
                 BeginDrawing();
                 ClearBackground(WHITE);
+
+                //Game Logic Begins
                 DrawCircle(circle_x, circle_y, 25, RED);
+                DrawRectangle(axe_x, axe_y, 50, 50, BLUE);
+
+                //move the axe
+                axe_y += direction;
+
+                if (axe_y > 450 || axe_y < 0 )
+                {
+                        direction = -direction;
+                }
 
                 if (IsKeyDown(KEY_D) && circle_x <= (_screenWidth - 50))
                 {
-                        circle_x = circle_x + 10;
+                        circle_x += 10;
                 }
 
                 if (IsKeyDown(KEY_A) && circle_x >= 50)
                 {
-                        circle_x = circle_x - 10;
+                        circle_x -= 10;
                 }
 
                 EndDrawing();
