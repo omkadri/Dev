87b054647d5e005d45fff4a8bf93ec301e751f69
Omar <omkadri@hotmail.com>
2025-07-12 21:40:18 -0400

    Raylib: updated template to include screen properties + added textures for dapper dasher game

---
+++ Raylib/dapper-dasher/dasher.cpp
@@ -0,0 +1,23 @@
+#include "raylib.h"
+int main()
+{ 
+        //window properties
+        const int _windowWidth = 800;
+        const int _windowHeight = 450;
+        int _targetFramerate = 60;
+
+        //initialize window
+        InitWindow(_windowWidth, _windowHeight, "Dapper Dasher");
+        SetTargetFPS(_targetFramerate);
+        
+        while(!WindowShouldClose())
+        {
+                //Start Drawing
+                BeginDrawing();
+                ClearBackground(WHITE);
+
+                //Stop Drawing
+                EndDrawing();
+        }
+        CloseWindow();
+}
