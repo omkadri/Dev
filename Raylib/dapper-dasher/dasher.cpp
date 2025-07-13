02eac7a77bfd45d8eda8e5e71e1703086c35766b
Omar <omkadri@hotmail.com>
2025-07-12 21:58:28 -0400

    Raylib: Created foundation for dasher dapper jump logic

--- Raylib/dapper-dasher/dasher.cpp
+++ Raylib/dapper-dasher/dasher.cpp
@@ -6,17 +6,34 @@ int main()
         const int _windowHeight = 450;
         int _targetFramerate = 60;
 
+        //player properties
+        const int _playerWidth = 50;
+        const int _playerHeight = 50;
+        int _playerPosY = _windowHeight - _playerHeight;
+        int _playerVelocityY = 0;
+        int _playerJumpVelocity = 10;
+
         //initialize window
         InitWindow(_windowWidth, _windowHeight, "Dapper Dasher");
         SetTargetFPS(_targetFramerate);
         
         while(!WindowShouldClose())
         {
-                //Start Drawing
+                //start drawing
                 BeginDrawing();
                 ClearBackground(WHITE);
 
-                //Stop Drawing
+                //player logic
+                DrawRectangle( _windowWidth/2, _playerPosY, _playerWidth, _playerHeight, BLUE );
+                
+                if (IsKeyPressed(KEY_SPACE))
+                {
+                        _playerVelocityY -= _playerJumpVelocity;
+                }
+                
+                _playerPosY += _playerVelocityY;
+
+                //stop drawing
                 EndDrawing();
         }
         CloseWindow();
