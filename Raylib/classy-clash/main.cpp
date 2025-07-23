c02c33d3bc3e93bbd3f5f81af6ff76d9fc23a326
Omar <omkadri@hotmail.com>
2025-07-23 19:49:39 -0400

    Raylib: Classy Clash: Map now moves according to player input and movement speed

--- Raylib/classy-clash/main.cpp
+++ Raylib/classy-clash/main.cpp
@@ -1,4 +1,6 @@
 #include "raylib.h"
+#include "raymath.h"
+
 int main()
 { 
         //window properties
@@ -11,15 +13,30 @@ int main()
         SetTargetFPS(_targetFramerate);
 
         Texture2D _worldMap = LoadTexture("assets/maps/worldMap/WorldMap.png");
-        
+        Vector2 _worldMapPos = {0, 0};
+        float _mapMoveSpeed{4.0};
+
         while(!WindowShouldClose())
         {
                 //Start Drawing
                 BeginDrawing();
                 ClearBackground(WHITE);
-                
+
+                Vector2 direction{};
+                if (IsKeyDown(KEY_A))
+                {
+                        direction.x -= 1.0f;
+                }
+                if (IsKeyDown(KEY_A)) direction.x -= 1.0f;
+                if (IsKeyDown(KEY_D)) direction.x += 1.0f;
+                if (IsKeyDown(KEY_W)) direction.y -= 1.0f;
+                if (IsKeyDown(KEY_S)) direction.y += 1.0f;
+                if (Vector2Length(direction) != 0.0)
+                {
+                        _worldMapPos = Vector2Subtract(_worldMapPos, Vector2Scale( Vector2Normalize(direction) * _mapMoveSpeed, _mapMoveSpeed));
+                }
+
                 //Draw the world map
-                Vector2 _worldMapPos = {0, 0};
                 DrawTextureEx(_worldMap, _worldMapPos, 0.0f, 4.0f, WHITE);
 
                 //Stop Drawing
