f70656d8c5f156b101572d83d71b003eb44de71e
Omar <omkadri@hotmail.com>
2025-07-23 19:35:44 -0400

    Raylib: Classy Clash: Imported sprites + Drew World Map

--- Raylib/classy-clash/main.cpp
+++ Raylib/classy-clash/main.cpp
@@ -2,19 +2,25 @@
 int main()
 { 
         //window properties
-        const int _windowWidth = 800;
-        const int _windowHeight = 450;
+        const int _windowWidth = 384;
+        const int _windowHeight = 384;
         int _targetFramerate = 60;
 
         //initialize window
-        InitWindow(_windowWidth, _windowHeight, "New Window");
+        InitWindow(_windowWidth, _windowHeight, "Classy Clash");
         SetTargetFPS(_targetFramerate);
+
+        Texture2D _worldMap = LoadTexture("assets/maps/worldMap/WorldMap.png");
         
         while(!WindowShouldClose())
         {
                 //Start Drawing
                 BeginDrawing();
                 ClearBackground(WHITE);
+                
+                //Draw the world map
+                Vector2 _worldMapPos = {0, 0};
+                DrawTextureEx(_worldMap, _worldMapPos, 0.0f, 4.0f, WHITE);
 
                 //Stop Drawing
                 EndDrawing();
