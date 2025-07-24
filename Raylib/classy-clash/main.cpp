fcff88f92df7c07dc53bc7fe76318b5fffad26a4
Omar <omkadri@hotmail.com>
2025-07-23 20:08:44 -0400

    Raylib: Classy Clash: Drew the player using DrawTexturePro

--- Raylib/classy-clash/main.cpp
+++ Raylib/classy-clash/main.cpp
@@ -12,9 +12,20 @@ int main()
         InitWindow(_windowWidth, _windowHeight, "Classy Clash");
         SetTargetFPS(_targetFramerate);
 
+        //map properties
         Texture2D _worldMap = LoadTexture("assets/maps/worldMap/WorldMap.png");
         Vector2 _worldMapPos = {0, 0};
         float _mapMoveSpeed{4.0};
+
+        //player properties
+        Texture2D _playerSpritesheet = LoadTexture("assets/characters/knight_idle_spritesheet.png");
+        int _playerSpritesheetRowCount = 6;
+        int _playerSpritesheetColumnCount = 1;
+        float _playerScale = 4.0f;
+        Vector2 _playerPos = { 
+                (float)_windowWidth / 2.0f - _playerScale * (0.5 * (float)_playerSpritesheet.width / _playerSpritesheetRowCount), 
+                (float)_windowHeight / 2.0f - _playerScale * ( 0.5f * (float)_playerSpritesheet.height / _playerSpritesheetColumnCount)
+        };
 
         while(!WindowShouldClose())
         {
@@ -38,6 +49,12 @@ int main()
 
                 //Draw the world map
                 DrawTextureEx(_worldMap, _worldMapPos, 0.0f, 4.0f, WHITE);
+
+                //Draw the player
+                Rectangle _playerSourceRec = {0.0f, 0.0f, (float)_playerSpritesheet.width / _playerSpritesheetRowCount, (float)_playerSpritesheet.height / _playerSpritesheetColumnCount};
+                Rectangle _playerDestRec = {_playerPos.x, _playerPos.y, _playerScale * (float)_playerSpritesheet.width / _playerSpritesheetRowCount, _playerScale * (float)_playerSpritesheet.height / _playerSpritesheetColumnCount};
+                
+                DrawTexturePro(_playerSpritesheet, _playerSourceRec, _playerDestRec, (Vector2){}, 0.0f, WHITE);
 
                 //Stop Drawing
                 EndDrawing();
