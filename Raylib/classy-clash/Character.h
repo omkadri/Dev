ac54531f5cf2e451692d8486daf0268d4743855e
Omar <omkadri@hotmail.com>
2025-08-18 16:26:10 -0400

    Raylib: Classy Clash: Enemy know chases player via toTarget vector

--- Raylib/classy-clash/Character.h
+++ Raylib/classy-clash/Character.h
@@ -8,6 +8,7 @@ class Character : public BaseCharacter
 public:
         Character(int windowWidth, int windowHeight);
         virtual void tick(float deltaTime) override;
+        Vector2 getScreenPos() { return screenPos; }
 
 private:
 };
