b0d3e7e9c075322d81a3597601262ccc91856b3d
Omar <omkadri@hotmail.com>
2025-09-01 13:44:00 -0400

    RogueDeckBuilder: Cards now have a proper data source setup via MVVM pattern
    - Created a simple CardData scriptable object with basic properties
    - Created Card Model that uses card data
    - Updated cardViewCreator script to use card model as a parameter
    - Test system now uses card data scriptable object to populate the cardView

--- Unity/RogueDeckBuilder/Assets/Scripts/Systems/TestSystem.cs
+++ Unity/RogueDeckBuilder/Assets/Scripts/Systems/TestSystem.cs
@@ -4,11 +4,14 @@ public class TestSystem : MonoBehaviour
 {
     [SerializeField] HandView _handView;
 
+    [SerializeField] CardData _cardData;
+
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            CardView cardView = CardViewCreator.Instance.CreateCardView(transform.position, Quaternion.identity);
+            Card card = new(_cardData);
+            CardView cardView = CardViewCreator.Instance.CreateCardView(card, transform.position, Quaternion.identity);
             StartCoroutine(_handView.AddCard(cardView));
         }
     }
--- Unity/RogueDeckBuilder/Assets/Scripts/Views/CardView.cs
+++ Unity/RogueDeckBuilder/Assets/Scripts/Views/CardView.cs
@@ -8,4 +8,15 @@ public class CardView : MonoBehaviour
     [SerializeField] TMP_Text _mana;
     [SerializeField] SpriteRenderer _imageSpriteRenderer;
     [SerializeField] GameObject _wrapper;
+
+    public Card Card { get; private set; }
+
+    public void Setup(Card card)
+    {
+        Card = card;
+        _title.text = card.Title;
+        _description.text = card.Description;
+        _mana.text = card.Mana.ToString();
+        _imageSpriteRenderer.sprite = card.Image;
+    }
 }
