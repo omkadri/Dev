c461a3ef0191410aa35ec5aa200a130767d1f4a5
Omar <omkadri@hotmail.com>
2025-09-01 11:44:26 -0400

    RogueDeckBuilder: Started RogueDeckBuilder prototype. Created Basic Card Prefab

---
+++ Unity/RogueDeckBuilder/Assets/Scripts/Views/CardView.cs
@@ -0,0 +1,11 @@
+using TMPro;
+using UnityEngine;
+
+public class CardView : MonoBehaviour
+{
+    [SerializeField] TMP_Text _title;
+    [SerializeField] TMP_Text _description;
+    [SerializeField] TMP_Text _mana;
+    [SerializeField] SpriteRenderer _imageSpriteRenderer;
+    [SerializeField] GameObject _wrapper;
+}
