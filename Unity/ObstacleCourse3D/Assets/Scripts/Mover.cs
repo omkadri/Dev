3c378eba050b563a1ace74de8a7b3ef4d8c206b2
Omar <omkadri@hotmail.com>
2025-09-26 17:01:42 -0400

    Unity: ObstacleCourse3D: Added Classic Input handling and Cinemachine camera

--- Unity/ObstacleCourse3D/Assets/Scripts/Mover.cs
+++ Unity/ObstacleCourse3D/Assets/Scripts/Mover.cs
@@ -2,14 +2,17 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] float _speed = 10f;
+
     void Start()
     {
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(0.01f, 0f, 0f);
+        float _xValue = Input.GetAxis("Horizontal") * Time.deltaTime * _speed;
+        float _yValue = 0f;
+        float _zValue = Input.GetAxis("Vertical") * Time.deltaTime * _speed;
+        transform.Translate(_xValue, _yValue, _zValue);
     }
 }
