ea032ee372f843f4f2ee74bb1b3a56b2edb89841
Omar <omkadri@hotmail.com>
2025-10-08 22:05:56 -0400

    Unity: Added Art to Assets. Added parallax background

---
+++ Unity/BraidOut/Assets/Scripts/ScrollingShooter2D/BackgroundParallaxScroller.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class BackgroundParallaxScroller : MonoBehaviour
+{
+    [SerializeField] Vector2 _moveSpeed;
+
+    Vector2 _offset;
+    Material _material;
+
+
+    void Awake()
+    {
+        _material = GetComponent<SpriteRenderer>().material;
+    }
+
+
+    void Update()
+    {
+        _offset = _moveSpeed * Time.deltaTime;
+        _material.mainTextureOffset += _offset;
+        Debug.Log(_offset);
+    }
+}
