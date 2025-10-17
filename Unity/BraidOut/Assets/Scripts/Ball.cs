03f637cceb8e944dc786b83d6cf39873e829bfb1
Omar <omkadri@hotmail.com>
2025-10-17 19:42:36 -0400

    Unity: BraidOut: Added Raycast to ball script to prevent issue where ball could travel through bricks

--- Unity/BraidOut/Assets/Scripts/Ball.cs
+++ Unity/BraidOut/Assets/Scripts/Ball.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    [SerializeField] float _speed = 5f;
+    [SerializeField] float _speed = 40f;
     [SerializeField] Vector2 _direction = new Vector2(1f, 1f); // Initial direction
     [SerializeField] bool _isActivated = false;
 
@@ -13,24 +13,50 @@ public class Ball : MonoBehaviour
 
     void Update()
     {
-        if (_isActivated)
-        {
-            Vector3 movement = (Vector3)(_direction * _speed * Time.deltaTime); //TODO: Understand why we cast
-            transform.position += movement;
-
-            Vector3 pos = transform.position;
-            transform.position = pos;
-        }
-        else
+        if (!_isActivated)
         {
             if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonUp(0))
             {
-                transform.SetParent(null); //This detaches the ball from the paddle, allowing for independent movement.
+                transform.SetParent(null); // Detach from paddle
                 _isActivated = true;
             }
         }
-        
     }
+
+    void FixedUpdate()
+    {
+        if (_isActivated)
+        {
+            float distance = _speed * Time.fixedDeltaTime;
+            Vector2 currentPosition = transform.position;
+
+            // Check for a collision in the direction of movement
+            RaycastHit2D hit = Physics2D.Raycast(currentPosition, _direction, distance);
+
+            if (hit.collider != null)
+            {
+                // Move the ball to the point of collision
+                transform.position = hit.point;
+
+                // Check if the object hit implements IDeflector
+                IDeflector deflector = hit.collider.GetComponent<IDeflector>();
+                if (deflector != null)
+                {
+                    _direction = deflector.GetDeflection(transform.position, _direction).normalized;
+                }
+
+                // Optional: Slightly nudge forward to avoid getting stuck at the hit point
+                transform.position += (Vector3)(_direction * (_speed * 0.01f));
+            }
+            else
+            {
+                // No collision detected, move normally
+                Vector3 movement = (Vector3)(_direction * distance);
+                transform.position += movement;
+            }
+        }
+    }
+
 
     void OnTriggerEnter2D(Collider2D other)
     {
