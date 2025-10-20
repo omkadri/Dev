5f2f9835fc6cdc25d71361d9d4bf9a9ed8cda4f0
Omar <omkadri@hotmail.com>
2025-10-20 16:35:09 -0400

    Unity: BraidOut: Implemented a fix for issue where ball would get stuck in an endless deflection loop between left and right wall

--- Unity/BraidOut/Assets/Scripts/WallDeflector.cs
+++ Unity/BraidOut/Assets/Scripts/WallDeflector.cs
@@ -4,11 +4,24 @@ public class WallDeflector : MonoBehaviour, IDeflector
 {
     [SerializeField] bool _reflectX = true;
     [SerializeField] bool _reflectY = false;
+    
+    [SerializeField] float _horizontalThreshold = 0.01f;
+    [SerializeField] float _verticalNudge = 0.2f;
 
     public Vector2 GetDeflection(Vector2 ballPosition, Vector2 ballDirection)
     {
         if (_reflectX) ballDirection.x *= -1f;
         if (_reflectY) ballDirection.y *= -1f;
+
+        if (Mathf.Abs(ballDirection.y) < _horizontalThreshold)
+        {
+            float xSign = Mathf.Sign(ballDirection.x);
+            if (xSign == 0f)
+                xSign = 1f;
+
+            ballDirection.y = _verticalNudge * xSign;
+        }
+
         return ballDirection.normalized;
     }
 }
