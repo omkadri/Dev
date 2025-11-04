dec5231b3c0b896602fc4bd82e6bd9c8f3afbea7
Omar <omkadri@hotmail.com>
2025-11-04 12:12:53 -0500

    Unity: ObstacleCourse: Coverted setup to use new input system + player know turns to face moving direction

--- Unity/ObstacleCourse3D/Assets/Scripts/Player.cs
+++ Unity/ObstacleCourse3D/Assets/Scripts/Player.cs
@@ -1,18 +1,66 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [Header("Movement Settings")]
     [SerializeField] float _speed = 10f;
 
-    void Start()
+    [Header("Rotation Settings")]
+    [SerializeField] bool _snapTurning = false; // Toggle for instant rotation
+    [SerializeField] float _turnRotationSpeed = 10f; // Speed for smooth rotation
+
+    InputActions _inputActions;
+    Vector2 _moveInput;
+
+    void Awake()
     {
+        _inputActions = new InputActions();
+    }
+
+    void OnEnable()
+    {
+        _inputActions.Player.Enable();
+        _inputActions.Player.Move.performed += OnMove;
+        _inputActions.Player.Move.canceled += OnMove;
+    }
+
+    void OnDisable()
+    {
+        _inputActions.Player.Move.performed -= OnMove;
+        _inputActions.Player.Move.canceled -= OnMove;
+        _inputActions.Player.Disable();
+    }
+
+    void OnMove(InputAction.CallbackContext context)
+    {
+        _moveInput = context.ReadValue<Vector2>();
     }
 
     void Update()
     {
-        float _xValue = Input.GetAxis("Horizontal") * Time.deltaTime * _speed;
-        float _yValue = 0f;
-        float _zValue = Input.GetAxis("Vertical") * Time.deltaTime * _speed;
-        transform.Translate(_xValue, _yValue, _zValue);
+        Vector3 move = new Vector3(_moveInput.x, 0f, _moveInput.y);
+
+        transform.Translate(move * _speed * Time.deltaTime, Space.World);
+
+        if (move.sqrMagnitude > 0.001f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(move);
+
+            if (_snapTurning)
+            {
+                // Instantly face the direction of movement
+                transform.rotation = targetRotation;
+            }
+            else
+            {
+                // Smoothly rotate towards movement direction
+                transform.rotation = Quaternion.Slerp(
+                    transform.rotation,
+                    targetRotation,
+                    _turnRotationSpeed * Time.deltaTime
+                );
+            }
+        }
     }
 }
