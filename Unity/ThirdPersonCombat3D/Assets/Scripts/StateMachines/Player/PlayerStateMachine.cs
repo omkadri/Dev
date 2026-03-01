be8b68a4da4d885438205b45899b1b5affe9a4ff
Omar Kadri <omkadri@hotmail.com>
2026-03-01 14:15:40 -0500

    ThirdPersonCombat3D: Implemented test for input events in state machine

--- Unity/ThirdPersonCombat3D/Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
+++ Unity/ThirdPersonCombat3D/Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class PlayerStateMachine : StateMachine
 {
+    [field: SerializeField] public InputHandler InputHandler { get; set; }// [field: SerializeField] exposes the propetries of this getter to the unity inspector.
+
     void Start()
     {
         SwitchState(new PlayerTestState(this));
--- Unity/ThirdPersonCombat3D/Assets/Scripts/StateMachines/Player/PlayerTestState.cs
+++ Unity/ThirdPersonCombat3D/Assets/Scripts/StateMachines/Player/PlayerTestState.cs
@@ -1,23 +1,33 @@
+using System.Threading;
 using UnityEngine;
 
 public class PlayerTestState : PlayerBaseState
 {
+    float _timer = 0f;
+
     public PlayerTestState(PlayerStateMachine stateMachine) : base(stateMachine)
     {
     }
 
     public override void Enter()
     {
-        Debug.Log("Enter");
+        _stateMachine.InputHandler.JumpEvent += OnJump;
     }
 
     public override void Tick(float deltaTime)
     {
-        Debug.Log("Tick");
+        _timer += deltaTime;
+
+        Debug.Log(_timer);
     }
 
     public override void Exit()
     {
-        Debug.Log("Exit");
+        _stateMachine.InputHandler.JumpEvent -= OnJump;
+    }
+
+    void OnJump()
+    {
+        _stateMachine.SwitchState(new PlayerTestState(_stateMachine));
     }
 }
