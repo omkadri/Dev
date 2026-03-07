dc5adfa11fa84a8b6766e1dfd19f03a0051b9d81
Omar Kadri <omkadri@hotmail.com>
2026-03-07 11:05:48 -0500

    ThirdPersonCombat3D: Added player rotation smoothing + refactored player state code
    - renamed PlayerTestState to PlayerFreeLookState
    - removed hardcoded strings and magic numbers
    - Created FaceMovementDirection() functions that rotates player according to a damping value

---
+++ Unity/ThirdPersonCombat3D/Assets/Scripts/StateMachines/Player/PlayerFreeLookState.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class PlayerFreeLookState : PlayerBaseState
+{
+    readonly int FreeLookSpeedHash = Animator.StringToHash("FreeLookSpeed"); //integers are processed faster than strings.
+    const float AnimatorDampTime = 0.075f;
+    public PlayerFreeLookState(PlayerStateMachine stateMachine) : base(stateMachine)
+    {
+    }
+
+    public override void Enter()
+    {
+    }
+
+    public override void Tick(float deltaTime)
+    {
+        Vector3 movement = CalculateMovement();
+        _stateMachine.CharacterController.Move(movement * _stateMachine.FreeLookMovementSpeed * deltaTime);
+
+        if (_stateMachine.InputHandler.MovementValue == Vector2.zero)
+        {
+            _stateMachine.Animator.SetFloat(FreeLookSpeedHash, 0, AnimatorDampTime, deltaTime); //TODO: Fix magic numbers
+            return;
+        }
+
+        _stateMachine.Animator.SetFloat(FreeLookSpeedHash, 1, AnimatorDampTime, deltaTime); //TODO: Fix magic numbers
+
+        FaceMovementDirection(movement, deltaTime);
+    }
+
+    public override void Exit()
+    {
+    }
+
+    Vector3 CalculateMovement()
+    {
+        Vector3 forward = _stateMachine.MainCameraTransform.forward;
+        forward.y = 0;
+        forward.Normalize();
+
+        Vector3 right = _stateMachine.MainCameraTransform.right;
+        right.y = 0;
+        right.Normalize();
+
+        // Combine camera forward/right directions with input so movement is relative to the camera's facing direction
+        return (forward * _stateMachine.InputHandler.MovementValue.y) + (right * _stateMachine.InputHandler.MovementValue.x);
+    }
+
+    void FaceMovementDirection(Vector3 movement, float deltaTime)
+    {
+        _stateMachine.transform.rotation = Quaternion.Lerp(
+            _stateMachine.transform.rotation,
+            Quaternion.LookRotation(movement),
+            deltaTime * _stateMachine.RotationDamping);
+    }
+}
--- Unity/ThirdPersonCombat3D/Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
+++ Unity/ThirdPersonCombat3D/Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -6,12 +6,13 @@ public class PlayerStateMachine : StateMachine
     [field: SerializeField] public CharacterController CharacterController { get; set; }
     [field: SerializeField] public Animator Animator { get; set; }
     [field: SerializeField] public float FreeLookMovementSpeed { get; set; }
+    [field: SerializeField] public float RotationDamping { get; set; }
     public Transform MainCameraTransform { get; set; }
 
     void Start()
     {
         MainCameraTransform = Camera.main.transform;
 
-        SwitchState(new PlayerTestState(this));
+        SwitchState(new PlayerFreeLookState(this));
     }
 }
