3feba9acd2d9996c44fd568b40e15b4388873579
Omar Kadri <omkadri@hotmail.com>
2026-03-28 22:30:25 -0400

    ThirdPersonCombat3D: Setup Enemy Idle State with blend trees

---
+++ Unity/ThirdPersonCombat3D/Assets/Scripts/StateMachines/Enemy/EnemyIdleState.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class EnemyIdleState : EnemyBaseState
+{
+    readonly int LocomotionBlendTreeHash = Animator.StringToHash("Locomotion");
+    readonly int SpeedHash = Animator.StringToHash("Speed");//TODO: Come up with beeter name for 'Speed' animator variable
+
+    const float CrossFadeDuration = 0.2f;
+    const float AnimatorDampTime = 0.1f;
+
+
+    public EnemyIdleState(EnemyStateMachine stateMachine) : base(stateMachine)
+    {
+    }
+
+    public override void Enter()
+    {
+        _stateMachine.Animator.CrossFadeInFixedTime(LocomotionBlendTreeHash, CrossFadeDuration);
+        _stateMachine.Animator.SetFloat(SpeedHash, 0);
+    }
+
+    public override void Tick(float deltaTime)
+    {
+        _stateMachine.Animator.SetFloat(SpeedHash, 0, AnimatorDampTime, deltaTime);
+    }
+
+    public override void Exit()
+    {
+    }
+}
--- Unity/ThirdPersonCombat3D/Assets/Scripts/StateMachines/Enemy/EnemyStateMachine.cs
+++ Unity/ThirdPersonCombat3D/Assets/Scripts/StateMachines/Enemy/EnemyStateMachine.cs
@@ -2,4 +2,10 @@ using UnityEngine;
 
 public class EnemyStateMachine : StateMachine
 {
+    [field: SerializeField] public Animator Animator { get; set; }
+
+    void Start()
+    {
+        SwitchState(new EnemyIdleState(this));
+    }
 }
