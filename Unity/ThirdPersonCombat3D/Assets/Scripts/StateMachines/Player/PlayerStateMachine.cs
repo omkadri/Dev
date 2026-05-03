75bf431483d15e73d614c91ba720ebc190d6bacb
Omar Kadri <omkadri@hotmail.com>
2026-05-03 15:54:29 -0400

    ThirdPersonCombat3D: Cursor is now hidden when game begins

--- Unity/ThirdPersonCombat3D/Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
+++ Unity/ThirdPersonCombat3D/Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -24,6 +24,9 @@ public class PlayerStateMachine : StateMachine
 
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
         MainCameraTransform = Camera.main.transform;
 
         SwitchState(new PlayerFreeLookState(this));
