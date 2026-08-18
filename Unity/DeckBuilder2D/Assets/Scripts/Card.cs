969a8d7e3a0bb93283e9ac9c093b13f87957a14f
Omar Kadri <omkadri@hotmail.com>
2026-08-17 20:11:14 -0400

    Unity: DeckBuilder2D: Cards can now be dragged and snapped back into place

--- Unity/DeckBuilder2D/Assets/Scripts/Card.cs
+++ Unity/DeckBuilder2D/Assets/Scripts/Card.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
 
 public class Card : MonoBehaviour
@@ -18,9 +19,10 @@ public class Card : MonoBehaviour
     [SerializeField] float _hoverOffest = 2f;
 
     Vector3 _originalScale;
-    Vector3 _originalPos;
+    Vector3 _originalPosition;
     SortingGroup _sortingGroup;
     int _originalSortingOrder;
+    static bool _isBeingDragged = false; //static prevents inactive cards from expanding on hover during drag
 
     void Awake()
     {
@@ -30,7 +32,7 @@ public class Card : MonoBehaviour
     void Start()
     {
         _originalScale = transform.localScale;
-        _originalPos = transform.localPosition;
+        _originalPosition = transform.localPosition;
         _originalSortingOrder = _sortingGroup.sortingOrder;
     }
 
@@ -44,6 +46,11 @@ public class Card : MonoBehaviour
 
     void OnMouseEnter() //TODO: Support New Input System only (not Both)
     {
+        if (_isBeingDragged)
+        {
+            return;
+        }
+
         transform.localScale = _originalScale * _hoverScale;
         transform.localPosition += new Vector3(0, _hoverOffest, 0f);
         _sortingGroup.sortingOrder += 1;
@@ -51,8 +58,34 @@ public class Card : MonoBehaviour
 
     void OnMouseExit() //TODO: Support New Input System only (not Both)
     {
+        if (_isBeingDragged)
+        {
+            return;
+        }
+
         transform.localScale = _originalScale;
-        transform.localPosition = _originalPos;
+        transform.localPosition = _originalPosition;
+        _sortingGroup.sortingOrder = _originalSortingOrder;
+    }
+
+    void OnMouseDrag()
+    {
+        _isBeingDragged = true;
+        gameObject.transform.position = GetMousePosistion();
+    }
+
+    Vector3 GetMousePosistion()//TODO: Abstract into Utils class???
+    {
+        Vector3 mousePosition = Mouse.current.position.ReadValue();
+        mousePosition.z = transform.position.z - Camera.main.transform.position.z;//Prevents cared from being clipped outside of camera
+        return Camera.main.ScreenToWorldPoint(mousePosition);
+    }
+
+    void OnMouseUp()
+    {
+        _isBeingDragged = false;
+        transform.localScale = _originalScale;
+        transform.localPosition = _originalPosition;
         _sortingGroup.sortingOrder = _originalSortingOrder;
     }
 }
