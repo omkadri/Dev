a05c28ebdf94bcb2e86c472b7e6a13fcaf300dbb
Omar Kadri <omkadri@hotmail.com>
2026-08-23 18:43:18 -0400

    Unity: DeckBuilder2D: Player now plays attack animation when attack card is played.
    - created player attack animation clip
    - created attack function in Player script that moves the player back and forth while triggering the attack animation.
    - player animator moves back to idling by default when animation is finished.
    - fixed issue where hero  sprite sheets were incorrectly sliced

--- Unity/DeckBuilder2D/Assets/Scripts/Player.cs
+++ Unity/DeckBuilder2D/Assets/Scripts/Player.cs
@@ -1,7 +1,22 @@
+using System.Collections;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] GameObject _playerSprite;
+    Vector3 _originalPosition;
+    Animator _animator;
+
+    void Awake()
+    {
+        _animator = _playerSprite.GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        _originalPosition = _playerSprite.transform.position;
+    }
+
     void OnEnable()
     {
         PlayerEvents.OnCardPlayed += HandleCardPlayed;
@@ -15,5 +30,49 @@ public class Player : MonoBehaviour
     void HandleCardPlayed(CardData cardData)
     {
         Debug.Log("Handler Ran");
+        if(cardData.AttackPower > 0)
+        {
+            Attack(cardData);
+        }
+    }
+
+    void Attack(CardData cardData)
+    {
+        Debug.Log("Attack: " + cardData.AttackPower);
+        StartCoroutine(PlayerAttackAnimRoutine());
+    }
+
+    IEnumerator PlayerAttackAnimRoutine()
+    {
+        Vector3 targerPosition = _originalPosition + new Vector3(4f, 0, 0); //TODO: Magic Number
+
+        float duration = 0.5f;//TODO: Magic Number
+        float timeElapsed = 0f;//TODO: Magic Number
+
+        //move forward
+        while(timeElapsed < duration)
+        {
+            _playerSprite.transform.position = Vector3.Lerp(_originalPosition, targerPosition, timeElapsed/duration);
+            timeElapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        _animator.Play("Attack"); //TODO: expose as serialized field???
+
+        yield return new WaitForSeconds(0.5f); //TODO: Magic number. maybe a more systematic approach???
+
+        timeElapsed = 0;
+
+        //move back to starting position
+        while(timeElapsed < duration)
+        {
+            _playerSprite.transform.position = Vector3.Lerp(targerPosition, _originalPosition, timeElapsed/duration);
+            timeElapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        yield return null;
     }
 }
