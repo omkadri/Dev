7ea0b1138d067343c1a3a75450093907ad8590ad
Omar Kadri <omkadri@hotmail.com>
2026-09-08 22:03:14 -0400

    Unity: UI-Toolkit: Creating project. Importing UTIF package from course

---
+++ Unity/UI-Toolkit/Assets/Scripts/FlickeringLight.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace GameDevTV.AS9BH
+{
+    [RequireComponent(typeof(Light))]
+    public class FlickeringLight : MonoBehaviour
+    {
+        [SerializeField] private AnimationCurve brightnessCurve;
+        [SerializeField] private float flickerSpeed = 0.1f;
+
+        private float baseIntensity;
+        private Light light;
+
+        private void Awake()
+        {
+            light = GetComponent<Light>();
+        }
+
+        private IEnumerator Start()
+        {
+            float time = 0;
+
+            baseIntensity = light.intensity;
+            while (enabled)
+            {
+                light.intensity = brightnessCurve.Evaluate(time) * baseIntensity;
+                yield return null;
+                time += Time.deltaTime * flickerSpeed;
+
+                if (time > 1)
+                {
+                    time = 0;
+                }
+            }
+        }
+    }
+}
